7fe0c00dd098ce5abd1ec17ff1d5d0008c0635fb
Jermaine Davis <jermaine_davis1@hotmail.com>
2016-07-13 17:14:14 -0400

    Change made here

--- src/app/pikaday/pikaday.component.ts
+++ src/app/pikaday/pikaday.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 
 //Place this at the top near your imports
 /// <reference path="../../../typings/globals/pikaday/index.d.ts" />
-declare var Pikaday;
+declare var Pikaday; //unsure how to initialized with property 'field'
+
+/// <reference path="../../../typings/globals/underscore/index.d.ts" />
+declare var _;
+
 
 @Component({
   moduleId: module.id,
@@ -13,5 +17,5 @@ declare var Pikaday;
 })
 
 export class PikadayComponent {
-
+  _.each([1, 2, 3], alert);
 }
